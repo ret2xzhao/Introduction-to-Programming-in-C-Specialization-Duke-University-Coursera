879b1e00daa12a2a91f93c36e2f03ee4bf594c11
Coursera Learner <nobody@nowhere.nul>
2019-01-16 09:21:36 +0000

    Did assignment 5

--- 05_squares/squares.c
+++ 05_squares/squares.c
@@ -20,38 +20,36 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
     //count from 0 to w. Call the number you count with x
     for (int x = 0; x < w; x++) {
       //check if  EITHER
-      //    ((x is between x_offset  and x_offset +size2) AND 
+      //    ((x is between x_offset  and x_offset +size2) AND
       //     y is equal to either y_offset OR y_offset + size2 - 1 )
       //  OR
       //    ((y is between y_offset and y_offset + size2) AND
       //     x is equal to either x_offset OR x_offset + size2 -1)
       // if so, print a *
-      if ((x_offset <= x && x < x_offset + size2 -1) && (y == y_offset || y == y_offset + size2 - 1)) {
-	printf("*");
+      if (((x >= x_offset)&&(x < x_offset+size2))&&((y == y_offset)||(y == y_offset+size2-1))) {
+        printf("*");
       }
-      if ((y_offset <= y && y < y_offset + size2 -1) && (x == x_offset || x == x_offset + size2 - 1)) {
-	printf("*");
+      if (((y_offset <= y)&&(y < y_offset+size2))&&((x == x_offset)||(x == x_offset+size2-1))) {
+        printf("*");
       }
       //if not,
-      else {
       // check if EITHER
       //    x is less than size1 AND (y is either 0 or size1-1)
       // OR
       //    y is less than size1 AND (x is either 0 or size1-1)
       //if so, print a #
-	if (x <= size1 && (y == 0 || y == size1 -1)) {
-	  printf("#");
-	}
-	if (y <= size1 && (x == 0 || x == size1 -1)) {
-	  printf("#");
-        }
-      //else print a space
-	else {
-	  printf(" ");
-        }
+      if (x <= size1 && (y == 0 || y == size1-1)) {
+        printf("#");
       }
-    //when you finish counting x from 0 to w, 
-    //print a newline
+      if (y <= size1 && (x == 0 || x == size1-1)) {
+        printf("#");
+      }
+      //else print a space
+      else {
+        printf(" ");
+      }
+      //when you finish counting x from 0 to w,
+      //print a newline
       printf("\n");
     }
   }
