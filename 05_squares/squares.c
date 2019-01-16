64fa4d7a25d2127169a3d1f799f8b7155e25d596
Coursera Learner <nobody@nowhere.nul>
2019-01-16 22:39:12 +0000

    Did assignment 5

--- 05_squares/squares.c
+++ 05_squares/squares.c
@@ -14,14 +14,16 @@ int between(int num, int min, int max) {
   if (num >= min && num < max) {
     return 1;
   } 
-  return 0;
+  else {
+    return 0;
+  }
 }
 
 void squares(int size1, int x_offset, int y_offset, int size2) {
   //compute the max of size1 and (x_offset + size2).  Call this w
-  int w = maximum(size1, x_offset + size2);
+  int w = maximum(size1, (x_offset + size2));
   //compute the max of size1 and (y_offset + size2).  Call this h
-  int h = maximum(size1, y_offset + size2);
+  int h = maximum(size1, (y_offset + size2));
   //count from 0 to h. Call the number you count with y
   for (int y = 0; y < h; y++) {
     //count from 0 to w. Call the number you count with x
@@ -33,7 +35,7 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
       //    ((y is between y_offset and y_offset + size2) AND
       //     x is equal to either x_offset OR x_offset + size2 -1)
       // if so, print a *
-      if ((between(x, x_offset, x_offset+2) && (y == y_offset || y == y_offset+size2-1)) || (between(y, y_offset, y_offset+2) && (x == x_offset || x == x_offset+size2-1))) {
+      if ((between(x, x_offset, x_offset+size2) && (y == y_offset || y == y_offset+size2-1)) || (between(y, y_offset, y_offset+size2) && (x == x_offset || x == x_offset+size2-1))) {
         printf("*");
       }
       //if not,
@@ -42,12 +44,12 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
       // OR
       //    y is less than size1 AND (x is either 0 or size1-1)
       //if so, print a #
-      else if ((x <= size1 && (y == 0 || y == size1-1)) || (y <= size1 && (x == 0 || x == size1-1))) {
+      else if ((x < size1 && (y == 0 || y == size1-1)) || (y < size1 && (x == 0 || x == size1-1))) {
 	printf("#");
       }
       //else print a space
       else {
-	printf(" ");
+        printf(" ");
       }
     }
     //when you finish counting x from 0 to w,
