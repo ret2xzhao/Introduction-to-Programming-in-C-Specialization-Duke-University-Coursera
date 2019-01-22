af804f8ffbde2b43247e78784bcc242afa37429b
Coursera Learner <nobody@nowhere.nul>
2019-01-22 08:29:55 +0000

    Did assignment 7

--- 07_retirement/retirement.c
+++ 07_retirement/retirement.c
@@ -1,40 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct retire_info {
+struct _retire_info {
   int months;
   double contribution;
   double rate_of_return;
-}
-  typedef struct retire_info retire_info;
+};
+typedef struct _retire_info retire_info;
 
 void print_current_balance (int months, double balance) {
-  printf("Age %3d month %2d you have $%.2lf\n", month/12, month%12, balance);
-}
-  
-balance = initial;
-for (int i = 0; i < retire_info working.months; i++) {
-  balance = balance + balance * retire_info working.rate_of_return;
-  balance = balance + retire_info working.contribution;
-  print_current_balance (int months, double balance);
- }
-
-for (int j = 0; j < retire_info retired.months; j++) {
-  balance = balance + balance * retire_info retired.rate_of_return;
-  balance = balance + retire_info retired.spending;
-  print_current_balance (int months, double balance);
- }
-
-double balance () {
+  printf("Age %3d month %2d you have $%.2lf\n", months/12, months%12, balance);
 }
 
 void retirement (int startAge, double initial, retire_info working, retire_info retired)
+  double balance = initial;
+int total_months = startAge;
+  
+for (int i = 0; i < retire_info working.months; i++) {
+  print_current_balance (int total_months, double balance);
+  balance = balance + balance * retire_info working.rate_of_return;
+  balance = balance + retire_info working.contribution;
+  total_months = total_months + 1;
+  
+  return balance;
+ }
 
-  int main(void) {
+for (int j = 0; j < retire_info retired.months; j++) {
+  print_current_balance (int total_months, double balance);
+  balance = balance + balance * retire_info retired.rate_of_return;
+  balance = balance + retire_info retired.spending;
+  total_months = total_months + 1;
+
+  return balance;
+ }
+  
+int main(void) {
+  retire_info working;
   working.months = 489;
   working.contribution = 1000;
   working.rate_of_return = 0.045 / 12;
 
+  retire_info retired;
   retired.months = 384;
   retired.spending = -4000;
   retired.rate_of_return = 0.01 / 12;
