73c446ccadbc6da34b3dd15ebbdca7c75cd5f607
Coursera Learner <nobody@nowhere.nul>
2019-01-22 09:02:27 +0000

    Did assignment 7

--- 07_retirement/retirement.c
+++ 07_retirement/retirement.c
@@ -10,29 +10,31 @@ typedef struct _retire_info retire_info;
 
 void print_current_balance (int months, double balance) {
   printf("Age %3d month %2d you have $%.2lf\n", months/12, months%12, balance);
+  return;
 }
 
-void retirement (int startAge, double initial, retire_info working, retire_info retired)
+void retirement (int startAge, double initial, retire_info working, retire_info retired) {
   double balance = initial;
-int total_months = startAge;
+  int total_months = startAge;
   
-for (int i = 0; i < retire_info working.months; i++) {
-  print_current_balance (int total_months, double balance);
-  balance = balance + balance * retire_info working.rate_of_return;
-  balance = balance + retire_info working.contribution;
-  total_months = total_months + 1;
+  for (int i = 0; i < working.months; i++) {
+    print_current_balance (total_months, balance);
+    balance = balance + balance * working.rate_of_return;
+    balance = balance + working.contribution;
+    total_months = total_months + 1;
   
-  return balance;
- }
+    return balance;
+  }
 
-for (int j = 0; j < retire_info retired.months; j++) {
-  print_current_balance (int total_months, double balance);
-  balance = balance + balance * retire_info retired.rate_of_return;
-  balance = balance + retire_info retired.spending;
-  total_months = total_months + 1;
-
-  return balance;
- }
+  for (int j = 0; j < retired.months; j++) {
+    print_current_balance (total_months, balance);
+    balance = balance + balance * retired.rate_of_return;
+    balance = balance + retired.contribution;
+    total_months = total_months + 1;
+  
+    return balance;
+  }
+}
   
 int main(void) {
   retire_info working;
@@ -42,9 +44,9 @@ int main(void) {
 
   retire_info retired;
   retired.months = 384;
-  retired.spending = -4000;
+  retired.contribution = -4000;
   retired.rate_of_return = 0.01 / 12;
 
-  retirement (327, 21345, retire_info working, retire_info retired);
+  retirement (327, 21345, working, retired);
   return 0;
 }
