9ca913b4a5ceced1ef0ad29a40ffeb84d1ad1cc8
Coursera Learner <nobody@nowhere.nul>
2019-01-29 19:58:41 +0000

    Did assignment c2prj1

--- c2prj1_cards/cards.c
+++ c2prj1_cards/cards.c
@@ -63,25 +63,25 @@ void print_card(card_t c) {
 card_t card_from_letters(char value_let, char suit_let) {
   card_t temp;
   switch(value_let) {
-  case '2' : temp.value = 2; break;
-  case '3' : temp.value = 3; break;
-  case '4' : temp.value = 4; break;
-  case '5' : temp.value = 5; break;
-  case '6' : temp.value = 6; break;
-  case '7' : temp.value = 7; break;
-  case '8' : temp.value = 8; break;
-  case '9' : temp.value = 9; break;
-  case '0' : temp.value = 10; break;
-  case 'J' : temp.value = VALUE_JACK; break;
-  case 'Q' : temp.value = VALUE_QUEEN; break;
-  case 'K' : temp.value = VALUE_KING; break;
-  case 'A' : temp.value = VALUE_ACE; break;
+  case '2': temp.value = 2; break;
+  case '3': temp.value = 3; break;
+  case '4': temp.value = 4; break;
+  case '5': temp.value = 5; break;
+  case '6': temp.value = 6; break;
+  case '7': temp.value = 7; break;
+  case '8': temp.value = 8; break;
+  case '9': temp.value = 9; break;
+  case '0': temp.value = 10; break;
+  case 'J': temp.value = VALUE_JACK; break;
+  case 'Q': temp.value = VALUE_QUEEN; break;
+  case 'K': temp.value = VALUE_KING; break;
+  case 'A': temp.value = VALUE_ACE; break;
   }
   switch(suit_let) {
-  case 's' : temp.suit = SPADES; break;
-  case 'h' : temp.suit = HEARTS; break;
-  case 'd' : temp.suit = DIAMONDS; break;
-  case 'c' : temp.suit = CLUBS; break;
+  case 's': temp.suit = SPADES; break;
+  case 'h': temp.suit = HEARTS; break;
+  case 'd': temp.suit = DIAMONDS; break;
+  case 'c': temp.suit = CLUBS; break;
   }
   assert_card_valid(temp);
   return temp;
