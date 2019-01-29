a014ccd6cbd10d6bed9997b940bc1fac9c2b4f7f
Coursera Learner <nobody@nowhere.nul>
2019-01-29 17:54:15 +0000

    Did assignment c2prj1

--- c2prj1_cards/cards.c
+++ c2prj1_cards/cards.c
@@ -26,10 +26,10 @@ const char * ranking_to_string(hand_ranking_t r) {
 
 char value_letter(card_t c) {
   switch(c.value) {
-  case 2: return "2"; break;
-  case 3: return "3"; break;
-  case 4: return "4"; break;
-  case 5: return "5"; break;
+  case 2: return '2'; break;
+  case 3: return '3'; break;
+  case 4: return '4'; break;
+  case 5: return '5'; break;
   case 6: return "6"; break;
   case 7: return "7"; break;
   case 8: return "8"; break;
