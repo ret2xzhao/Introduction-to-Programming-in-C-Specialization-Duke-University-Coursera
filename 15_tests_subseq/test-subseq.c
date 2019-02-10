082937a31ddfb08075e21906ae7e7693890fc13b
Coursera Learner <nobody@nowhere.nul>
2019-02-10 22:45:42 +0000

    Did assignment 15

---
+++ 15_tests_subseq/test-subseq.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+size_t maxSeq(int * array, size_t n);
+
+int main(void) {
+  int array1[] = {}
+  int array2[] = {1, 2, 1, 3, 5, 7, 2, 4, 6, 9}
+  int array3[] = {2, 4, 67, 93, 94, 102}
+
+  return EXIT_SUCCESS;
+}
