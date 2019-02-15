5ebee190fe1733ef23bb815fab6d0b157646f230
Coursera Learner <nobody@nowhere.nul>
2019-02-15 17:52:02 +0000

    Did assignment 18

--- 18_reverse_str/reverse.c
+++ 18_reverse_str/reverse.c
@@ -4,6 +4,23 @@
 
 void reverse(char * str) {
   //WRITE ME!
+  const char * p1 = str;
+  char temp;
+  int length = strlen(str);
+  int i;
+  int j = length - 1;
+  if (length = 0) {
+    return "";
+   }
+   else {
+     for (i = 0; i < (length-1); i++) {
+       temp = str[j];
+       str[j] = str[i];
+       str[i] = temp;
+       j--;
+     }
+     return str;
+   }
 }
 
 int main(void) {
