42dd2fd422ad662d0e06fe7459dc50b0e5240d9c
Coursera Learner <nobody@nowhere.nul>
2019-02-25 23:59:32 +0000

    Did assignment 23

---
+++ 23_power_rec/power.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+unsigned power(unsigned x, unsigned y) {
+  
+  if (x=0, y=0) {
+    return 1;
+  }
+  
+  if (x=0, y>0) {
+    return 0;
+  }  
+  
+  if (x>0, y=0) {
+    return 1;
+  }
+  
+  else {
+    int ans = x;
+    if (y=1) {
+      return ans;
+    }
+    
+    else {
+      ans = power(x, y-1);
+      return ans * x;
+    }  
+  }
+}
+
+int main(void) {
+  //test_case_1
+  if(power(0, 0) != 1) {
+    return EXIT_FAILURE;
+  }
+
+  //test_case_2
+  if(power(0, 1) != 0) {
+    return EXIT_FAILURE;
+  }
+  
+  //test_case_3
+  if(power(1, 0) != 1) {
+    return EXIT_FAILURE;
+  }
+  
+  //test_case_4
+  if(power(1, 1) != 1) {
+    return EXIT_FAILURE;
+  }
+  
+  //test_case_5
+  if(power(3, 0) != 1) {
+    return EXIT_FAILURE;
+  }
+  
+  //test_case_6
+  if(power(0, 3) != 0) {
+    return EXIT_FAILURE;
+  }
+  
+  //test_case_7
+  if(power(2, 8) != 256) {
+    return EXIT_FAILURE;
+  }
+  
+  //test_case_8
+  if(power(10, 6) != 1000000) {
+    return EXIT_FAILURE;
+  }
+  
+  //test_case_9
+  if(power(-2, 2) != 4) {
+    return EXIT_FAILURE;
+  }
+  
+  return EXIT_SUCCESS;
+}
