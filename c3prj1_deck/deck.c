c4ea519d87aa916ada33c01f1a87bbf0f76f4bca
Coursera Learner <nobody@nowhere.nul>
2019-02-28 00:30:04 +0000

    Did assignment c3prj1_deck

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -37,16 +37,6 @@ void shuffle(deck_t * d) {
 }
 
 void assert_full_deck(deck_t * d) {
-  card_t ** ptr = d->cards;
-  deck_t temp_deck;
-  temp_deck.cards = d->cards;
-  for(int i=0; i<(d->n_cards); i++) {
-    card_t temp_card = **ptr;
-    assert_card_valid(temp_card);
-    if(i>0){
-      temp_deck.n_cards = (size_t)i;
-      assert(!deck_contains(&temp_deck, temp_card));
-    }
-    ptr++;
-  }
+  for (int i = 0; i < d->n_cards; i++)
+    assert(deck_contains(d, card_from_num(i)));  
 }
