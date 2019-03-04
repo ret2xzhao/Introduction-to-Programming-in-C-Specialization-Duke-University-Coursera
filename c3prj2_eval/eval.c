fbe161363ff16a3bb03b17339d96360e603fe693
Coursera Learner <nobody@nowhere.nul>
2019-03-04 03:28:28 +0000

    Added files

--- c3prj2_eval/eval.c
+++ c3prj2_eval/eval.c
@@ -190,8 +190,8 @@ int compare_hands(deck_t * hand1, deck_t * hand2) {
 */
 
 int compare_hands(deck_t * hand1, deck_t * hand2) {
-  qsort(hand1->cards, hand1->n_cards, sizeof(card_t *), card_ptr_comp);
-  qsort(hand2->cards, hand2->n_cards, sizeof(card_t *), card_ptr_comp);
+  qsort(hand1->cards, hand1->n_cards, sizeof(hand1->cards[0]), card_ptr_comp);
+  qsort(hand2->cards, hand2->n_cards, sizeof(hand2->cards[0]), card_ptr_comp);
 
   hand_eval_t hand_1 = evaluate_hand(hand1);
   hand_eval_t hand_2 = evaluate_hand(hand2);
