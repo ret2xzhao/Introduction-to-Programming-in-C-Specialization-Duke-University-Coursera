e1431c5f04811666d96d8a3965fb6083e76c4137
Coursera Learner <nobody@nowhere.nul>
2019-03-08 08:50:33 +0000

    Did assignment 25

--- 25_break_encr/breaker.c
+++ 25_break_encr/breaker.c
@@ -1,43 +1,85 @@
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <ctype.h>
 
-void frequency_count(FILE * f) {
+
+
+void getFreqArray(int *freqArr, FILE * f){
+  
   int c;
-  int frequency_count_array[26] = {0};
-  while ((c = fgetc(f)) != EOF) {
-    if (isalpha(c)) {
+  
+  while((c=fgetc(f)) != EOF){
+    
+    if(isalpha(c)){
+      
       c = tolower(c);
-      frequency_count_array[c-'a']=frequency_count_array[c-'a']+1;
+      
+      c = c - 'a';
+      
+      freqArr[c] = freqArr[c]+1;
+      
     }
+    
   }
+  
 }
 
-void find_key(int * frequency_count_array) {
-  int x;
-  int key;
-  int max = 0;
-  for(x=0; x<26; x++) {
-    if(frequency_count_array[x] > max) {
-      max = frequency_count_array[x];
+
+
+int maxFreqIdx(int * freqArr){
+  
+  int max = freqArr[0];
+  
+  int maxIdx = 0;
+  
+  for(int i=0; i<26; i++){
+    
+    if(freqArr[i]>max){
+      
+      max = freqArr[i];
+      
+      maxIdx = i;
+      
     }
+    
   }
-  key = (max + 'a' - 'e' + 26)%26;
-  printf("%d\n", key);
+  
+  return maxIdx;
+  
 }
 
-int main(int argc, char ** argv) {
-  if (argc != 2) {
-    fprintf(stderr,"Not enough arguments");
+
+
+int main(int argc, char ** argv){
+  
+  if(argc!=2){
+    
+    fprintf(stderr, "Usage: Wrong amount of Arguments\n");
+    
     return EXIT_FAILURE;
+    
   }
-  FILE * f = fopen(argv[1], "r");
-  if (f == NULL) {
+  
+  FILE * f = fopen(argv[1],"r");
+  
+  if(f==NULL){
+    
     perror("Could not open file");
+    
     return EXIT_FAILURE;
+    
   }
-  if (fclose(f) != 0) {    
-    perror("Failed to close the input file!");    
-    return EXIT_FAILURE; 
-  }
+  
+  int freqArr[26] = {0};
+  
+  getFreqArray(freqArr, f);
+  
+  int maxIdx = maxFreqIdx(freqArr);
+  
+  int key = (maxIdx + 'a' - 'e' + 26)%26;
+  
+  printf("%d\n", key);
+  
 }
