9b67ddb1cae3c6ad8b5b447e42aaed69fefa1ef3
Coursera Learner <nobody@nowhere.nul>
2019-03-11 17:34:54 +0000

    Did assignment 27

--- 27_matrix_input/rotateMatrix.c
+++ 27_matrix_input/rotateMatrix.c
@@ -1,78 +1,89 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define LINE_SIZE 12
-#define DIMENSION 10
 
-/*Matrix Rotation Function*/
-void rotate(char matrix[10][10]) {
-  int i;
-  int j;
-  char temp;
-  for (i=0; i<5; i++) {
-    for (j=i; j<(9-i); j++) {
-      temp = matrix[i][j];
-      matrix[i][j] = matrix[9-j][i];
-      matrix[9-j][i] = matrix[9-i][9-j];
-      matrix[9-i][9-j] = matrix[j][9-i];
-      matrix[j][9-i] = temp;
-    }
-  }
-}
+int LINE_LENGTH = 12;
+int MATRIX_DIM = 10;
 
 int main(int argc, char ** argv) {
   if (argc != 2) {
-    fprintf(stderr,"Not enough arguments");
+    fprintf(stderr, "Usage: rotateMatrix inputFileName\n");
     return EXIT_FAILURE;
   }
 
+  // open file
   FILE * f = fopen(argv[1], "r");
+  // make sure a file was opened.
   if (f == NULL) {
-    perror("Could not open file");
+    perror("Could not open file!");
     return EXIT_FAILURE;
   }
-
-  char line[LINE_SIZE];
-  char mat[DIMENSION][DIMENSION];
-  int height = 0;
-  while (fgets(line, LINE_SIZE, f) !=NULL) {
+  // check that the input is correct
+  int height = 0; // track how many lines matrix has
+  char line[LINE_LENGTH];
+  char matrix[MATRIX_DIM][MATRIX_DIM];
+  while(fgets(line, LINE_LENGTH, f) != NULL) {
+    // If line has more than 10 characters
     if (strchr(line, '\n') == NULL) {
-      fprintf(stderr, "Line %d is too long!\n", height);
+      fprintf(stderr, "Input matrix line (%d)'s width is more than 10 characters.\n", height);
       return EXIT_FAILURE;
     }
-    int new_line = 0;
-    for (int n=0; n<LINE_SIZE; n++) {
+    // If line has less than 10 characters
+    int whereIsNL = 0;
+    for (int n=0; n<LINE_LENGTH; n++) {
       if (line[n] == '\n') {
-	new_line = n;
+	whereIsNL = n;
       }
     }
-    if (new_line < 10) {
-      fprintf(stderr, "Input matrix line (%d)'s width is %d, which is less than 10 characters.\n", height, new_line);
+    if (whereIsNL < 10) {
+      fprintf(stderr, "Input matrix line (%d)'s width is %d, which is less than 10 characters.\n", height, whereIsNL);
       return EXIT_FAILURE;
     }
     for (int i=0; i<10; i++) {
-      mat[height][i] = line[i];
+      matrix[height][i] = line[i];
     }
+
     height++;
   }
-  if (height < DIMENSION) {
-    fprintf(stderr, "Input matrix's height is less than 10");
+  // if matrix has less than 10 rows
+  if(height < MATRIX_DIM) {
+    fprintf(stderr, "Input matrix height is %d (which is too small), but should be 10.", height-1);
     return EXIT_FAILURE;
   }
-  if (height > DIMENSION) {
-    fprintf(stderr, "Input matrix's height is more than 10");
+
+  // if matrix has more than 10 rows
+  if (height > MATRIX_DIM) {
+    fprintf(stderr, "Input matrix height is %d (which is too big), but should be 10.\n", height-1);
     return EXIT_FAILURE;
-  }  
-  rotate(mat);
-  for (int r=0; r<10; r++) {
-    for (int c=0; r<10; c++) {
-      printf("%c", mat[r][c]);
+  }
+  // if the file does not close
+  if (fclose(f) != 0) {
+    perror("Failed to close the file!");
+    return EXIT_FAILURE;
+  }
+
+  // rotate the matrix 90 degrees clockwise and print it to stdout
+  void rotate(char matrix[10][10]) {
+    int i;
+    int j;
+    char temp;
+    for (i=0; i<5; i++) {
+      for (j=i; j<(9-i); j++) {
+	temp = matrix[i][j];
+	matrix[i][j] = matrix[9-j][i];
+	matrix[9-j][i] = matrix[9-i][9-j];
+	matrix[9-i][9-j] = matrix[j][9-i];
+	matrix[j][9-i] = temp;
+      }
+    }
+  }
+
+  for (int r=0; r<MATRIX_DIM; r++) {
+    for (int c=0; c<MATRIX_DIM; c++) {
+      printf("%c", matrix[r][c]);
     }
     printf("\n");
   }
-  if (fclose(f) != 0) {    
-    perror("Failed to close the input file!");    
-    return EXIT_FAILURE;  
-  }
+  
   return EXIT_SUCCESS;
 }
