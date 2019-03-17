1ab1acf3a4d0cea86ebb7745df26a44f2b7159ca
Coursera Learner <nobody@nowhere.nul>
2019-03-17 21:52:05 +0000

    Did assignment 29

--- 29_outname/outname.c
+++ 29_outname/outname.c
@@ -5,4 +5,9 @@
 
 char * computeOutputFileName(const char * inputName) {
   //WRITE ME
+  char * outputName = malloc((strlen(inputName) + 6) * sizeof(*outputName));
+  strcpy(outputName, argv[2]);
+  strcat(outputName, ".counts");
+  free(outputName);
+  return;
 }
