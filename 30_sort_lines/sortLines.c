cf564a846c0fb37a6ad1d4942809e69e0af1f1d7
Coursera Learner <nobody@nowhere.nul>
2019-03-18 23:11:30 +0000

    Did assignment 30

--- 30_sort_lines/sortLines.c
+++ 30_sort_lines/sortLines.c
@@ -14,26 +14,6 @@ void sortData(char ** data, size_t count) {
   qsort(data, count, sizeof(char *), stringOrder);
 }
 
-void readData(FILE * f) {
-  char ** lines = NULL;
-  char * curr = NULL;
-  size_t sz;
-  size_t i = 0;
-  while (getline(&curr, &sz, stdin) >= 0) {
-    lines = realloc(lines, (i+1) * sizeof(*lines));
-    lines[i] = curr;
-    curr = NULL;
-    i++;
-  }
-  free(curr);
-  sortData(lines, i);
-  for (size_t j = 0; j < i; j++) {
-    printf("%s", lines[j]);
-    free(lines[j]);
-  }
-  free(lines);
-}
-
 int main(int argc, char ** argv) { 
   //WRITE YOUR CODE HERE!
   if (argc == 0) {
@@ -46,13 +26,30 @@ int main(int argc, char ** argv) {
       perror("Could not open file");
       return EXIT_FAILURE;
     }
-    readData(f);
+    char ** lines = NULL;
+    char * curr = NULL;
+    size_t sz;
+    size_t i = 0;
+    while (getline(&curr, &sz, stdin) >= 0) {
+      lines = realloc(lines, (i+1) * sizeof(*lines));
+      lines[i] = curr;
+      curr = NULL;
+      i++;
+    }
+    free(curr);
+    sortData(lines, i);
+    for (size_t j = 0; j < i; j++) {
+      printf("%s", lines[j]);
+      free(lines[j]);
+    }
+    free(lines);
     if (fclose(f) != 0) {    
       perror("Failed to close the input file!");    
       return EXIT_FAILURE;  
     }
     return EXIT_SUCCESS;
   }
+  
   if (argc >= 2) {
     for (int i=1; i<argc; i++) {
       FILE * f = fopen(argv[i], "r");
@@ -60,7 +57,23 @@ int main(int argc, char ** argv) {
         perror("Could not open file");
         return EXIT_FAILURE;
       }
-      readData(f);
+      char ** lines = NULL;
+      char * curr = NULL;
+      size_t sz;
+      size_t i = 0;
+      while (getline(&curr, &sz, stdin) >= 0) {
+        lines = realloc(lines, (i+1) * sizeof(*lines));
+        lines[i] = curr;
+        curr = NULL;
+        i++;
+      }
+      free(curr);
+      sortData(lines, i);
+      for (size_t j = 0; j < i; j++) {
+        printf("%s", lines[j]);
+        free(lines[j]);
+      }
+      free(lines);
       if (fclose(f) != 0) {    
         perror("Failed to close the input file!");    
         return EXIT_FAILURE;  
