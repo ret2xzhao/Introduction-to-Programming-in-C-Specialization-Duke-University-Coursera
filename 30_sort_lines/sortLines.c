e2cc0e502a301b336d64fc8c30fd75d47ae1f47a
Coursera Learner <nobody@nowhere.nul>
2019-03-18 22:50:51 +0000

    Did assignment 30

--- 30_sort_lines/sortLines.c
+++ 30_sort_lines/sortLines.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-
 //This function is used to figure out the ordering
 //of the strings in qsort.  You do not need
 //to modify it.
@@ -16,9 +14,54 @@ void sortData(char ** data, size_t count) {
   qsort(data, count, sizeof(char *), stringOrder);
 }
 
-int main(int argc, char ** argv) {
-  
+void readData(FILE * f) {
+  char ** lines = NULL;
+  char * curr = NULL;
+  size_t sz;
+  size_t i = 0;
+  while (getline(&curr, &sz, stdin) >= 0) {
+    lines = realloc(lines, (i+1) * sizeof(*lines));
+    lines[i] = curr;
+    curr = NULL;
+    i++;
+  }
+  free(curr);
+  sortData(lines, i);
+  for (size_t j = 0; j < i; j++) {
+    printf("%s", lines[j]);
+    free(lines[j]);
+  }
+  free(lines);
+}
+
+int main(int argc, char ** argv) { 
   //WRITE YOUR CODE HERE!
-  
-  return EXIT_SUCCESS;
+  if (argc == 1) {
+    FILE * f = fopen(argv[1], "r");
+    if (f == NULL) {
+      perror("Could not open file");
+      return EXIT_FAILURE;
+    }
+    readData(f);
+    if (fclose(f) != 0) {    
+      perror("Failed to close the input file!");    
+      return EXIT_FAILURE;  
+    }
+    return EXIT_SUCCESS;
+  }
+  if (argc >= 2) {
+    for (int i=1; i<argc; i++) {
+      FILE * f = fopen(argv[i], "r");
+      if (f == NULL) {
+        perror("Could not open file");
+        return EXIT_FAILURE;
+      }
+      readData(f);
+      if (fclose(f) != 0) {    
+        perror("Failed to close the input file!");    
+        return EXIT_FAILURE;  
+      }  
+    }
+    return EXIT_SUCCESS;
+  }
 }
