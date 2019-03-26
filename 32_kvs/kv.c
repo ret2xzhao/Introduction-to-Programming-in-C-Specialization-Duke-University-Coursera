5e14f07c8c995c6a0cf02f9baf3a5f23ea42791e
Coursera Learner <nobody@nowhere.nul>
2019-03-26 02:39:09 +0000

    Did assignment 32

--- 32_kvs/kv.c
+++ 32_kvs/kv.c
@@ -3,49 +3,62 @@
 #include <string.h>
 #include "kv.h"
 
-kvpair_t * getKVPair(char * line) {
-  char *key, *value;
+char * get_key (char * string) {
+  char * temp;
+  char * found = strdup(string);
+  temp = strsep(&found,"=");
+  return temp;  
+}
+
+char * get_value(char * string) {
+  char * found = strdup(string);
+  strsep(&found,"=");
+  return found;  
+}
+
+kvpair_t * get_pair(char * string) {
+  char * key;
+  char * value;
   kvpair_t * kvpair = malloc(sizeof(*kvpair));
-  key = strsep(&line, "=");
-  value = strsep(&line, "\n");
+  key = get_key (string);
+  value = get_value(string);
   kvpair->key = strdup(key);
   kvpair->value = strdup(value);
-  
   return kvpair;
 }
 
-void addPairToArray(kvarray_t * pairs, kvpair_t * kvpair) {
-  pairs->length++;
-  pairs->kvp_array = realloc(pairs->kvp_array, pairs->length * sizeof(*kvpair));
-  pairs->kvp_array[pairs->length-1] = kvpair;
+void add_pair_to_array(kvarray_t * kvarray, kvpair_t * kvpair) {
+  kvarray->length++;
+  kvarray->kvp_array = realloc(kvarray->kvp_array, kvarray->length * sizeof(*kvarray->kvp_array));
+  kvarray->kvp_array[kvarray->length - 1] = kvpair;
 }
 
 kvarray_t * readKVs(const char * fname) {
-  //WRITE ME
-  FILE * f = fopen(fname, "r");
+  FILE * f = fopen (fname, "r");
   if (f == NULL) {
     perror("fopen");
     fprintf(stderr, "Trying to open %s\n", fname);
     return NULL;
   }
-
-  kvarray_t * kvarray = malloc(sizeof(*kvarray));
+  
+  kvarray_t * kvarray = malloc(sizeof(*kvarray));  
+  kvarray->kvp_array = NULL;
+  kvarray->length = 0;
   char * line = NULL;
   size_t sz = 0;
-  while (getline(&line, &sz, f) > 0) {
-    kvpair_t * kvpair = getKVPair(line);
-    addPairToArray(kvarray, kvpair);
+  while(getline(&line, &sz, f)>0) {
+    kvpair_t * kvpair = get_pair(line);
+    add_pair_to_array(kvarray, kvpair);
     free(line);
     line = NULL;
   }
   free(line);
   
-  if (fclose(f) != 0) {
+  if (fclose(f) !=0) {
     perror("fclose");
     fprintf(stderr, "Trying to close %s\n", fname);
     return NULL;
   }
-
   return kvarray;
 }
 
