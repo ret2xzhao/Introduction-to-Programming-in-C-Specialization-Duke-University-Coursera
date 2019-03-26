074ac4f1d4cf1eded0fee484791d8e8d5e8f3354
Coursera Learner <nobody@nowhere.nul>
2019-03-26 02:02:19 +0000

    Did assignment 31

--- 32_kvs/kv.c
+++ 32_kvs/kv.c
@@ -3,58 +3,52 @@
 #include <string.h>
 #include "kv.h"
 
-kvpair_t * getKVPair(char * line) {
-  char *key, *value;
-  //const char delim = '=';
-  kvpair_t * kvpair = malloc(sizeof(*kvpair));
-
-  /*
-  //key = strsep(&line, &delim);
-  value = strchr(line, delim);
-  key = strsep(&line, &delim);
-  */
-  key = strsep(&line, "=");
-  value = strsep(&line, "\n");
-  kvpair->key = strdup(key);
-  kvpair->value = strdup(value);
-  
-  return kvpair;
+char * get_key (char * string) {
+  char * temp;
+  char * found = strdup(string);
+  temp = strsep(&found,"=");
+  return temp;  
 }
 
-void addPairToArray(kvarray_t * pairs, kvpair_t * kvpair) {
-  pairs->length++;
-  pairs->kvp_array = realloc(pairs->kvp_array, pairs->length * sizeof(*kvpair));
-  pairs->kvp_array[pairs->length-1] = kvpair;
+char * get_value(char * string) {
+  char * found = strdup(string);
+  strsep(&found,"=");
+  return found;  
+}
+
+void add_pair_to_array(kvarray_t * kvarray, kvpair_t * kvpair) {
+  kvarray->length++;
+  kvarray->kvp_array = realloc(kvarray->kvp_array, kvarray->length * sizeof(*kvarray->kvp_array));
+  kvarray->kvp_array[kvarray->length - 1] = kvpair;
 }
 
 kvarray_t * readKVs(const char * fname) {
-  //WRITE ME
-  FILE * f = fopen(fname, "r");
+  FILE * f = fopen (fname, "r");
   if (f == NULL) {
     perror("fopen");
     fprintf(stderr, "Trying to open %s\n", fname);
     return NULL;
   }
-
   kvarray_t * kvarray = malloc(sizeof(*kvarray));
-  kvarray->kvp_array = NULL;
-  kvarray->length = 0;
+  kvpair_t * kvpair = malloc(sizeof(*kvpair));
+  kvpair->key = NULL;
+  kvpair->value = NULL;
   char * line = NULL;
   size_t sz = 0;
-  while (getline(&line, &sz, f) > 0) {
-    kvpair_t * kvpair = getKVPair(line);
-    addPairToArray(kvarray, kvpair);
+  while(getline(&line, &sz, f)>0) {
+    kvpair->key = get_key (line);
+    kvpair->value = get_value(line);
+    add_pair_to_array(kvarray, kvpair);
     free(line);
     line = NULL;
   }
   free(line);
   
-  if (fclose(f) != 0) {
+  if (fclose(f) !=0) {
     perror("fclose");
     fprintf(stderr, "Trying to close %s\n", fname);
     return NULL;
   }
-
   return kvarray;
 }
 
