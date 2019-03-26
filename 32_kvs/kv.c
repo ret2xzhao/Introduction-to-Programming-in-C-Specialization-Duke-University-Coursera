dbfb85335f06a0be8eab7f2af0e50fb7a59ad479
Coursera Learner <nobody@nowhere.nul>
2019-03-26 17:51:46 +0000

    Did assignment 32

--- 32_kvs/kv.c
+++ 32_kvs/kv.c
@@ -49,15 +49,15 @@ kvarray_t * readKVs(const char * fname) {
   return kvarray;
 }
 
-void freeKVs(kvarray_t * pairs) {
+void freeKVs(kvarray_t * kvarray) {
   //WRITE ME
-  for (int i=0; i<pairs->length; i++) {
-    free(pairs->kvp_array[i]->key);
-    free(pairs->kvp_array[i]->value);
-    free(pairs->kvp_array[i]);
+  for (int i=0; i<kvarray->length; i++) {
+    free(kvarray->kvp_array[i]->key);
+    free(kvarray->kvp_array[i]->value);
+    free(kvarray->kvp_array[i]);
+    free(kvarray->kvp_array);
   }
-  free(pairs->kvp_array);
-  free(pairs);
+  free(kvarray);
 }
 
 void printKVs(kvarray_t * pairs) {
