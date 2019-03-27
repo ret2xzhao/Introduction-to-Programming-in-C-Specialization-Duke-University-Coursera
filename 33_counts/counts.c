5aa276415876363fcf8146b7b936ce4c7d3c6dee
Coursera Learner <nobody@nowhere.nul>
2019-03-27 17:44:36 +0000

    Did assignment 33

--- 33_counts/counts.c
+++ 33_counts/counts.c
@@ -1,49 +1,111 @@
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <string.h>
+
 #include "counts.h"
 
 counts_t * createCounts(void) {
+  
   //WRITE ME
+  
   counts_t * c = malloc(sizeof(*c));
-  c->one_count = NULL;
-  c->array_len = 0;
-  c->unknown_len = 0;
+  
+  c->one_count_array = malloc(sizeof(c->one_count_array));
+  
+  c->len_array = 0;
+  
+  c->len_unknowns = 0;
+  
   return c;
+  
 }
 
 void addCount(counts_t * c, const char * name) {
+  
   //WRITE ME
-  if(name = NULL) {
-    c->unknown_len++;
-    return;
+  
+  if (name == NULL) {
+    
+    c->len_unknowns++;
+    
   }
+  
   else {
-    for(int i=0; i<c->array_len; i++) {
-      if(strcmp(name, c->one_count[i]->string) == 0) {
-        c->one_count[i]->counter++;
+    
+    int in_array = 0;
+    
+    int i;
+    
+    for (i=0; i<c->len_array; i++) {
+      
+      if (strcmp(name, c->one_count_array[i]->some_string)==0) {
+	
+	in_array = 1;
+	
+	break; //want to save value of i to increment appropriate 'n_encounters'
+	
       }
+      
     }
+    
+    if (in_array==1) {
+      
+      c->one_count_array[i]->n_encounters++;
+      
+    }
+    
     else {
-      c->one_count = malloc(c->array_len * sizeof(*c->one_count));
-      c->array_len++;
-      c->one_count = realloc(c->one_counts, c->array_len * sizeof(*c->one_count));
-      c->one_count[c->array_len - 1]->string = name;
-      c->one_count[c->array_len - 1]->counter = 1;
+      
+      c->len_array++;
+      
+      c->one_count_array = realloc(c->one_count_array, c->len_array * sizeof(*c->one_count_array));
+      
+      c->one_count_array[c->len_array-1] = malloc(sizeof(one_count_t));
+      
+      c->one_count_array[c->len_array-1]->some_string = name;
+      
+      c->one_count_array[c->len_array-1]->n_encounters = 1;
+      
     }
+    
   }
+  
 }
 
 void printCounts(counts_t * c, FILE * outFile) {
+  
   //WRITE ME
-  for(int i=0; i<c->array_len; i++) {
-    fprintf(outFile, "%s: %d\n", c->one_count[i]->string, c->one_count[i]->counter);
+  
+  for (int i=0; i<c->len_array; i++) {
+    
+    fprintf(outFile, "%s: %d\n", c->one_count_array[i]->some_string, c->one_count_array[i]->n_encounters);
+    
   }
-  if(c->unknown_len>0) {
-    fprintf(outFile, "<unknown> : %d\n", c->unknown_len);
+  
+  if (c->len_unknowns > 0) {
+    
+    fprintf(outFile, "<unknown> : %d\n", c->len_unknowns);
+    
   }
+  
 }
 
+
+
 void freeCounts(counts_t * c) {
+  
   //WRITE ME
+  
+  for (int i=0; i<c->len_array; i++) {
+    
+    free(c->one_count_array[i]);
+    
+  }
+  
+  free(c->one_count_array);
+  
+  free(c);
+  
 }
--- 33_counts/counts.h
+++ 33_counts/counts.h
@@ -1,23 +1,47 @@
 #ifndef __COUNTS_H__
+
 #define __COUNTS_H__
+
 struct _one_count_t {
+  
   //DEFINE ME
-  char * string;
-  int counter;
+  
+  const char * some_string;
+  
+  int n_encounters;
+  
 };
+
 typedef struct _one_count_t one_count_t;
 
+
+
 struct _counts_t {
+  
   //DEFINE ME
-  one_count_t ** one_count;
-  int array_len;
-  int unknown_len;
+  
+  one_count_t ** one_count_array;
+  
+  int len_array;
+  
+  int len_unknowns;
+  
 };
+
 typedef struct _counts_t counts_t;
 
+
+
 counts_t * createCounts(void);
+
 void addCount(counts_t * c, const char * name);
+
 void printCounts(counts_t * c, FILE * outFile);
+
+
+
 void freeCounts(counts_t * c);
+
+
 
 #endif
