585d53d5373a4e07639d160d034acac8e4ec20da
Coursera Learner <nobody@nowhere.nul>
2019-03-27 07:03:32 +0000

    Did assignment 33

--- 33_counts/counts.c
+++ 33_counts/counts.c
@@ -5,10 +5,35 @@
 
 counts_t * createCounts(void) {
   //WRITE ME
+  counts_t * c = malloc(sizeof(*c));
+  c->one_count = NULL;
+  c->array_len = 0;
+  c->unknown_count = 0;
+  return c;
 }
+
 void addCount(counts_t * c, const char * name) {
   //WRITE ME
+  if(name = NULL) {
+    c->unknown_count++;
+    return;
+  }
+  else {
+    for(int i=0; i<c->array_len; i++) {
+      if(strcmp(name, c->one_count[i]->string) == 0) {
+        c->one_count[i]->counter++;
+      }
+    }
+    else {
+      c->one_count = malloc(c->array_len * sizeof(*c->one_count));
+      c->array_len++;
+      c->one_count = realloc(c->one_counts, c->array_len * sizeof(*c->one_count));
+      c->one_count[c->array_len - 1]->string = name;
+      c->one_count[c->array_len - 1]->counter = 1;
+    }
+  }
 }
+
 void printCounts(counts_t * c, FILE * outFile) {
   //WRITE ME
 }
