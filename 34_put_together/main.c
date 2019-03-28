f258b954fe40ce27a01191976314169962652352
Coursera Learner <nobody@nowhere.nul>
2019-03-28 19:10:12 +0000

    Did assignment 34

--- 34_put_together/main.c
+++ 34_put_together/main.c
@@ -21,14 +21,12 @@ counts_t * countFile(const char * filename, kvarray_t * kvPairs) {
     return NULL;
   }
   
-  counts_t * result = createCounts(void);
-  char * key;
-  char * value;
+  counts_t * result = createCounts();
   char * line = NULL;
   size_t sz = 0;
-  while(getline(&line, &sz, filename)>0) {
-    key = stripNewline(line);
-    value = lookupValue(kvPairs, key);
+  while(getline(&line, &sz, f)>0) {
+    stripNewline(line);
+    char * value = lookupValue(kvPairs, line);
     addCount(result, value);
     free(line);
     line = NULL;
@@ -37,55 +35,43 @@ counts_t * countFile(const char * filename, kvarray_t * kvPairs) {
   
   if (fclose(f) !=0) {
     perror("fclose");
-    fprintf(stderr, "Trying to close %s\n", fname);
+    fprintf(stderr, "Trying to close %s\n", filename);
     return NULL;
   }
   
   return result;
 }
-  
-int main(int argc, char ** argv) {
-  if (argc != 2) {
-    fprintf(stderr, "Usage: roster inputname\n");
-    return EXIT_FAILURE;
-  }
-  roster_t * the_roster = readInput(argv[1]);
-  if (r == NULL) {
-    fprintf(stderr,"Could not read the roster information\n");
-    return EXIT_FAILURE;
-  }
 
-  int main(int argc, char ** argv) {
-    //WRITE ME (plus add appropriate error checking!)
-    if (argc != 3) {
-      fprintf(stderr, "Usage: file inputname\n");
+int main(int argc, char ** argv) {
+  //WRITE ME (plus add appropriate error checking!)
+  if (argc < 2) {
+    fprintf(stderr, "Not enough arguments\n");
+    return EXIT_FAILURE;
+  }
+  //read the key/value pairs from the file named by argv[1] (call the result kv)
+  kvarray_t * kv = readKVs(argv[1]);
+  //count from 2 to argc (call the number you count i)
+  for (int i=2; i<argc; i++) {
+    //count the values that appear in the file named by argv[i], using kv as the key/value pair
+    counts_t * c=countFile(argv[i], kv);
+    //(call this result c)
+    //compute the output file name from argv[i] (call this outName)
+    char * outName = computeOutputFileName(argv[i]);
+    //open the file named by outName (call that f)
+    FILE * f = fopen (outName, "w");
+    //print the counts from c into the FILE f
+    printCounts(c, f);
+    //close f
+    if (fclose(f) !=0) {
+      perror("fclose");
+      fprintf(stderr, "Trying to close %s\n", outName);
       return EXIT_FAILURE;
     }
-    //read the key/value pairs from the file named by argv[1] (call the result kv)
-    kvarray_t * kv = readKVs(argv[1]);
-    void printKVs(kvarray_t * pairs);
-    //count from 2 to argc (call the number you count i)
-    for (int i=2; i<argc; i++) {
-      //count the values that appear in the file named by argv[i], using 
-      //kv as the key/value pair
-      char * lookupValue(kvarray_t * pairs, const char * key);
-      void addCount(counts_t * c, const char * name);
-      //   (call this result c)
-      //compute the output file name from argv[i] (call this outName)
-      char * outName = computeOutputFileName(const char * inputName) {
-	//open the file named by outName (call that f)
-	f = counts_t * createCounts(void);
-	//print the counts from c into the FILE f
-	printCounts(counts_t * c, FILE * outFile);
-	//close f
-	if (fclose(f) !=0) {
-	  perror("fclose");
-	  fprintf(stderr, "Trying to close %s\n", fname);
-	  return NULL;
-	}
-	//free the memory for outName and c
-	void freeCounts(counts_t * c);
-	//free the memory for kv
-	void freeKVs(kvarray_t * pairs);
-	return EXIT_SUCCESS;
-      }
+    //free the memory for outName and c
+    free(outName);
+    freeCounts(c);
+  }
+  //free the memory for kv
+  freeKVs(kv);
+  return EXIT_SUCCESS;
+}
