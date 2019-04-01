de59f8140b0eea56182a4a0a18bfdcc6f273b91e
Coursera Learner <nobody@nowhere.nul>
2019-04-01 00:59:30 +0000

    Did assignment c3prj2

--- c3prj2_eval/eval.c
+++ c3prj2_eval/eval.c
@@ -194,8 +194,24 @@ int compare_hands(deck_t * hand1, deck_t * hand2) {
   }
 }
 
-unsigned * get_match_counts(deck_t * hand) ;
-
+unsigned * get_match_counts(deck_t * hand) {
+  hand->cards
+  hand->n_cards
+    unsigned * array = malloc(hand->n_cards * sizeof(*array));
+  int counter = 0;
+  for(int i=14; i>0; i--) {
+    for(int j=0; j<hand->n_cards; j++) {
+      if(hand->cards[j].value == i) {
+        counter++;
+      }
+      for(int k=0; k<counter; k++) {
+	array[k] = counter;
+      }
+      counter = 0;
+    }
+  }
+  return array;
+}
 // We provide the below functions.  You do NOT need to modify them
 // In fact, you should not modify them!
 
