e1034929ca17b1fd59b487fb2b3115822c07ccbd
Coursera Learner <nobody@nowhere.nul>
2019-04-03 06:55:29 +0000

    Did assignment c4prj2

--- c4prj2_input/input.c
+++ c4prj2_input/input.c
@@ -1,73 +1,104 @@
-#include <string.h>
+#include<string.h>
+
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <assert.h>
+
 #include "deck.h"
+
 #include "cards.h"
+
 #include "future.h"
 
-deck_t * hand_from_string(const char * str, future_cards_t * fc) {
-  deck_t * deck = malloc(sizeof(*deck));
+deck_t * hand_from_string(const char * str, future_cards_t * fc){
+  
+  deck_t* deck=malloc(sizeof(*deck));
+  
   deck->cards = NULL;
-  deck->n_cards = 0;
-  for(int i=0; i<strlen(str); i++) {
-    if ((str[i] == ' ') || (str[i] == '\n')) {
-      continue;
-    }
-    else {
-      if(str[i] == '?') {
+  
+  deck->n_cards=0;
+  
+  for(int i=0;i<strlen(str);i++){
+    
+    if((str[i] == '\n')||(str[i] == ' '))continue;
+    
+    else{
+      
+      if(str[i] == '?'){
+	
         i++;
-        int j=0;
-        char index[2];
-        while (!((str[i] == ' ') || (str[i] == '\n'))) {
-          index[j] = str[i];
-        }
-        i++;
-        j++;
-        index[j] = '\0';
-        add_future_card(fc, atoi(index), add_empty_card(deck));
-      }
-      else {
-        add_card_to(deck, card_from_letters(str[i], str[i+1]));
-        i++;
-      }
-    }
-    if(deck->n_cards < 5) {
-      fprintf(stderr, "Not enough cards.");
-      return NULL;
-    }
-  }
-  return deck;
-}
+	
+        char num[strlen(str)];
+	
+        int n=0;
+	
+        while(!((str[i] == '\n')||(str[i] == ' '))) {
+	  
+          num[n]=str[i];
+	  
+          i++;n++;}
+	
+        num[n]='\0';
+	
+        add_future_card(fc,atoi(num),add_empty_card(deck)) ;
+	
 
-deck_t ** read_input(FILE * f, size_t * n_hands, future_cards_t * fc) {
-  FILE * input = fopen (f, "r");
-  if (input == NULL) {
-    perror("fopen");
-    fprintf(stderr, "Trying to open %s\n", f);
-    return NULL;
+	
+      }
+      
+      else{
+	
+        card_t x = card_from_letters(str[i],str[i+1]);
+	
+        add_card_to(deck,x);
+	
+        i++;}
+      
+    }
+    
   }
   
-  deck_t ** result = malloc(sizeof(**result));
-  result->cards = NULL;
-  result->n_cards = 0;
-  char * line = NULL;
-  size_t sz = 0;
-  int counter = 0;
-  while(getline(&line, &sz, input)>0) {
-    deck_t ** result = realloc(deck_t ** result, (result->n_cards+1) * sizeof(*deck_t ** result));
-    hand_from_string(line, fc);
-    free(line);
-    line = NULL;
-    n_cards++;
-  }
-  free(line);
-  n_hands = n_cards;
-
-  if (fclose(input) !=0) {
-    perror("fclose");
-    fprintf(stderr, "Trying to close %s\n", f);
+  if (deck->n_cards < 5 ){
+    
     return NULL;
+    
+    fprintf(stderr,"asas");}
+  
+  return deck;
+  
+}
+
+deck_t ** read_input(FILE * f, size_t * n_hands, future_cards_t * fc){
+  deck_t ** arr = malloc(sizeof(**arr));
+  
+  deck_t **arr=NULL;
+  
+  size_t n_hand=0;
+  
+  char *line=NULL;
+  
+  size_t sz=0;
+  
+  while(getline(&line,&sz,f)>=0){
+    
+    arr=realloc(arr,(n_hand+1)*sizeof(*arr));
+    
+    deck_t*deck=hand_from_string(line,fc);
+    
+    if (deck == NULL)continue;
+    
+    arr[n_hand]=deck;
+    
+    n_hand ++;
+    
   }
-  return result;
+  
+  free(line);
+  
+  *n_hands=n_hand;
+  
+  return arr;
+  
 }
