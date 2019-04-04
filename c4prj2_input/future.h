f7256900c92438f1743be373c36d86455904014b
Coursera Learner <nobody@nowhere.nul>
2019-04-04 07:18:07 +0000

    Did assignment c4prj2

--- c4prj2_input/future.h
+++ c4prj2_input/future.h
@@ -11,4 +11,4 @@ typedef struct future_cards_tag future_cards_t;
 void add_future_card(future_cards_t * fc, size_t index, card_t * ptr) ;
 void future_cards_from_deck(deck_t * deck, future_cards_t * fc);
 
-#endif
+#endif 
